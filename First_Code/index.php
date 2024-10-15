6db9be599fee6f0d350e1afd5dbefce1c3c7556c
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-10-16 01:50:41 +0600

    Learn how php embedded in html

--- First_Code/index.php
+++ First_Code/index.php
@@ -1,6 +1,17 @@
 <?php
-
-    echo 'Hello, PHP';
-
-
+    // echo 'Hello, PHP';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My First PHP file</title>
+</head>
+<body>
+    <h2>
+        <?php echo 'Hello from h2'; ?>
+    </h2>
+</body>
+</html>
