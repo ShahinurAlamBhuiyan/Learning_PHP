d505ce90d55ee134322b4a5695c06f6d2710a527
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-10-16 23:34:16 +0600

    Concat & String functions

---
+++ concat/index.php
@@ -0,0 +1,33 @@
+<?php
+    $firstName = 'Shahin ';
+    $lastName = 'Bhuiyan';
+
+    // echo $firstName.$lastName;
+
+    // echo 'Hey, my name is ' . $firstName.$lastName;
+    $message = 'Hey, my name is ' . $firstName.$lastName;
+    // echo $message;
+
+
+    // echo "Hey, my name is $firstName Alam $lastName";
+    // echo "3rd Character of first name is $firstName[2]";
+
+
+    // Functions ----->
+    // echo strlen($firstName);
+    // echo strtoupper($firstName);
+    // echo strtolower($firstName);
+    // echo str_replace('h', 'f', $firstName);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concat</title>
+</head>
+<body>
+    <h1> Learn Concat and Basic </h1>
+</body>
+</html>
