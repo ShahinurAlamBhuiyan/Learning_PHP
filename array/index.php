2a8bc0b84b02c1c7b69148bb74b356072a44296c
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-10-17 18:43:33 +0600

    array

--- array/index.php
+++ array/index.php
@@ -3,42 +3,42 @@
 	// indexed arrays
 
 	$peopleOne = ['shaun', 'crystal', 'ryu'];
-	//echo $peopleOne[1];
+	// echo $peopleOne[0];
 
 	$peopleTwo = array('ken', 'chun-li');
-	//echo $peopleTwo[1];
+	// echo $peopleTwo[1];
 
 	$ages = [20, 30, 40, 50];
 
-	//print_r($ages);
+	// print_r($ages);
 
 	$ages[1] = 25;
 	//print_r($ages);
 
 	$ages[] = 10;
 	array_push($ages, 70);
-	//print_r($ages);
+	// print_r($ages);
 
-	//echo array_pop($ages);
-	//print_r($ages);
+	// echo array_pop($ages);
+	// print_r($ages);
 
-	//echo count($ages);
+	// echo count($ages);
 
 	$peopleThree = array_merge($peopleOne, $peopleTwo);
-	//print_r($peopleThree);
+	// print_r($peopleThree);
 
 	// associative array (key & value pairs)
-
 	$ninjasOne = ['shaun' => 'black', 'mario' => 'orange', 'luigi' => 'brown'];
-	//echo $ninjasOne['mario'];
-	//print_r($ninjasOne);
+	// echo $ninjasOne['mario'];
+	// print_r($ninjasOne);
 
 	$ninjasTwo = array('bowser' => 'green', 'peach' => 'yellow');
-	//print_r($ninjasTwo);
+	// print_r($ninjasTwo);
 
 	$ninjasTwo['toad'] = 'pink';
+    // print_r($ninjasTwo);
 
-	//echo count($ninjasTwo);
+	// echo count($ninjasTwo);
 
 	$ninjasThree = array_merge($ninjasOne, $ninjasTwo);
 	print_r($ninjasThree);
